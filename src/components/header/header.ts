8e4db0ca4e115855ca4bf37e47744b5c8c79be8b
jinsil <jinsil378900@gmail.com>
2025-02-11 02:14:23 +0900

    feat: ViewportManager로 resize 전역으로 관리

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,5 +1,7 @@
 import { BREAKPOINT } from '../../constants/constant';
 import { debounce } from '../../utils/eventUtils';
+import viewportManager from '../../utils/viewportManager';
+import { getViewportType } from '../../utils/viewportUtils';
 import HeaderDesktop from './headerDesktop';
 import HeaderMobile from './headerMobile';
 
@@ -9,20 +11,39 @@ interface Props {
 }
 
 const Header = (props: Props) => {
+  const header = document.createElement('header');
+  header.className = 'header';
+
+  // 헤더 클릭 시 스크롤 상단으로 이동
+  header.onclick = event => {
+    if ((event.target as HTMLElement).tagName === 'HEADER') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  };
+
   const render = () => {
-    const isMobile = window.innerWidth <= BREAKPOINT.MOBILE;
-    return isMobile ? HeaderMobile(props) : HeaderDesktop(props);
+    header.innerHTML = '';
+    const content = getViewportType(window.innerWidth) === 'MOBILE' ? HeaderMobile(props) : HeaderDesktop(props);
+    header.appendChild(content);
   };
 
-  const handleResize = () => {
-    const header = document.querySelector('.header');
-    if (!header) return;
-    header.replaceWith(render());
+  const handleTrigger = (prevViewport: number, currentViewport: number) => {
+    return (
+      (getViewportType(prevViewport) === 'MOBILE' && getViewportType(currentViewport) !== 'MOBILE') ||
+      (getViewportType(prevViewport) !== 'MOBILE' && getViewportType(currentViewport) === 'MOBILE')
+    );
   };
 
-  window.addEventListener('resize', debounce(handleResize, 300));
+  const handleResize = debounce((currentViewport: number) => {
+    console.log('헤더 리렌더링', getViewportType(currentViewport));
+    render();
+  }, 300);
 
-  return render();
+  viewportManager.subscribe({ trigger: handleTrigger, callback: handleResize });
+
+  render();
+
+  return header;
 };
 
 export default Header;
---
+++ src/utils/viewportManager.js
@@ -0,0 +1,47 @@
+class ViewportManager {
+  constructor() {
+    this.listeners = new Set();
+    this.prevViewport = window.innerWidth;
+    this.resizeHandler = this.notifyListeners.bind(this);
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', this.resizeHandler);
+    }
+  }
+
+  /**
+   * 모든 구독자에게 viewport 변경을 알리는 함수
+   */
+  notifyListeners() {
+    const currentViewport = window.innerWidth;
+    this.listeners.forEach(({ trigger, callback }) => {
+      if (trigger(this.prevViewport, currentViewport)) {
+        callback(currentViewport);
+      }
+    });
+
+    this.prevViewport = currentViewport;
+  }
+
+  /**
+   * viewport 변경을 감지하는 구독 함수
+   */
+  subscribe({trigger, callback}) {
+    const listener = { trigger, callback };
+    this.listeners.add(listener);
+
+    return () => {
+      this.listeners.delete(listener);
+    };
+  }
+
+  /**
+   * 리스너 제거 및 window 이벤트 해제
+   */
+  destroy() {
+    window.removeEventListener('resize', this.resizeHandler);
+    this.listeners.clear();
+  }
+}
+
+export default new ViewportManager();
